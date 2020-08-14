a1155a0b4ed8ce57bed539b3a450fa982baf2473
Shafi Masoumi <masoumi2017@icould.com>
2020-08-13 17:58:23 -0700

    cockies ok now

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,16 +2,22 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
+def eating_cookies(n, cache = None):
     # base case
+    if n < 0:
+        return 0
+   
     if n == 0:
         return 1
     
-    elif n < 0:
-        return 0
 
-    else: 
-        return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+    elif cache is not None and cache[n] > 0:
+        return cache[n]
+    else:
+        if cache is None:
+            cache = [0 for i in range(n + 1)]
+        cache[n] = eating_cookies( n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+    return cache[n]
 
 
 if __name__ == "__main__":
